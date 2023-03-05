831bce6e9788caa446cdec33ce68dda16adc68f8
Ainadinov01~ <a.adilet.97@mail.ru>
2023-03-05 06:24:43 +0600

    Validation done

---
+++ main.js
@@ -0,0 +1,28 @@
+// Validation
+const email = document.querySelector(".newsletter__email");
+const icon1 = document.querySelector(".newsletter__icons");
+const btn = document.querySelector(".newsletter__btn");
+let redExp = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+
+email.addEventListener("keyup", function(e){
+    if(email.value.match(redExp)){
+        email.style.color = "#172432"
+        email.style.background = "#e8f0fe"
+        email.style.borderColor = "#e8f0fe"
+        icon1.style.display = "none"
+    }else{
+        email.style.color = "#e74c3c"
+        email.style.background = "#FFE8D4"
+        email.style.borderColor = "#e74c3c"
+        icon1.style.cssText = `
+            display: flex;
+            justify-content: space-around;
+            align-items: center;		`
+    }
+    if(email.value == ""){
+        email.style.borderColor = "lightgrey"
+        email.style.color = "#FF7757"
+        icon1.style.display = "none"
+    }
+});
